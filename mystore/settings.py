a57f0b5dc8e6cb4e2835075484d09db049d0a653
unuefeejovwoke <vuwill7114@gmail.com>
2022-10-15 20:04:46 -0700

    reset password and activate account

--- mystore/settings.py
+++ mystore/settings.py
@@ -138,7 +138,20 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'seu7.tech@gmail.com'
+EMAIL_HOST_PASSWORD ='rviookohpdqyynsl'
+EMAIL_USE_TLS = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
