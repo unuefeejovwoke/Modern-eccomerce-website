923f08faef78b67a9fc75b37877034661223c6d3
unuefeejovwoke <vuwill7114@gmail.com>
2022-10-16 18:56:05 -0700

    updated

--- mystore/settings.py
+++ mystore/settings.py
@@ -145,10 +145,10 @@ MESSAGE_TAGS = {
 }
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'seu7.tech@gmail.com'
-EMAIL_HOST_PASSWORD ='rviookohpdqyynsl'
-EMAIL_USE_TLS = True
+EMAIL_PORT = 
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD =''
+EMAIL_USE_TLS = 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
